85a4d4bdd8936505f23f40aa4916768757d38a11
Luca Marx <luca@lucamarx.com>
2023-02-21 09:50:03 +0100

    Updated tests

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -7,7 +7,7 @@ from fractions import Fraction
 from contfrac import ContFrac
 
 
-N_ITERS = 1_000
+N_ITERS = 10_000
 
 def test_creation_from_rational():
     "create a CF from a positive rational number"
@@ -17,8 +17,8 @@ def test_creation_from_rational():
 
         cv = a_cf.convergents_list()[-1]
 
-        assert cv[0] == a_rt.numerator
-        assert cv[1] == a_rt.denominator
+        assert cv.numerator == a_rt.numerator
+        assert cv.denominator == a_rt.denominator
 
 
 def test_creation_from_real():
@@ -28,6 +28,22 @@ def test_creation_from_real():
 
         z = ContFrac(x)
 
-        _, _, v = z.convergents_list()[-1]
+        cv = z.convergents_list()[-1]
 
-        assert pytest.approx(v, 1e-15) == x
+        assert pytest.approx(cv, 1e-15) == x
+
+
+def test_mobius():
+    "test Möbius transform"
+    for _ in range(N_ITERS):
+        a_rt = Fraction(random.randint(1, 1000), random.randint(1, 1000))
+        a_cf = ContFrac(a_rt)
+
+        a_cv = a_cf.convergents_list()[-1]
+
+        a, b, c, d = random.randint(1, 1000), random.randint(1, 1000), random.randint(1, 1000), random.randint(1, 1000)
+
+        b_cf = a_cf.mobius(a, b, c, d)
+        b_cv = b_cf.convergents_list()[-1]
+
+        assert Fraction((a + b*a_cv), (c + d*a_cv)) == b_cv
